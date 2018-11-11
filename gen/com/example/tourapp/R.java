1960cffade0722453dd4c7551efb6a2cd5e96cb0
Krzysztof Klimek <klimek.krzy@gmail.com>
2018-11-11 23:32:33 +0100

    add tour .xml

--- gen/com/example/tourapp/R.java
+++ gen/com/example/tourapp/R.java
@@ -25,20 +25,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_add_tour=0x7f08000b;
-        public static final int action_delete_tour=0x7f08000c;
-        public static final int action_logout=0x7f08000d;
-        public static final int action_settings=0x7f08000a;
-        public static final int checkBox=0x7f080009;
+        public static final int action_add_tour=0x7f080018;
+        public static final int action_delete_tour=0x7f080019;
+        public static final int action_logout=0x7f08001a;
+        public static final int action_settings=0x7f080017;
+        public static final int addTourButton=0x7f08000d;
+        public static final int checkBox=0x7f080016;
+        public static final int colon1=0x7f080005;
+        public static final int colon2=0x7f080007;
         public static final int listViewAdmin=0x7f080000;
-        public static final int listViewGuide=0x7f080001;
-        public static final int listViewTourist=0x7f080005;
-        public static final int login=0x7f080002;
-        public static final int loginButton=0x7f080004;
-        public static final int password=0x7f080003;
-        public static final int tourDate=0x7f080007;
-        public static final int tourDescription=0x7f080008;
-        public static final int tourName=0x7f080006;
+        public static final int listViewGuide=0x7f08000e;
+        public static final int listViewTourist=0x7f080012;
+        public static final int login=0x7f08000f;
+        public static final int loginButton=0x7f080011;
+        public static final int password=0x7f080010;
+        public static final int spinner=0x7f08000c;
+        public static final int tourDate=0x7f080014;
+        public static final int tourDateText=0x7f080003;
+        public static final int tourDayEdit=0x7f080004;
+        public static final int tourDescription=0x7f080015;
+        public static final int tourDescriptionEdit=0x7f08000a;
+        public static final int tourDescriptionText=0x7f080009;
+        public static final int tourGuideText=0x7f08000b;
+        public static final int tourMonthEdit=0x7f080006;
+        public static final int tourName=0x7f080013;
+        public static final int tourNameEdit=0x7f080002;
+        public static final int tourNameText=0x7f080001;
+        public static final int tourYearEdit=0x7f080008;
     }
     public static final class layout {
         public static final int activity_admin=0x7f030000;
